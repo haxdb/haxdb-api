2eddcb64d4aecfbc47ba70a52a2357336e2da3ee
jondale <github@serpco.com>
2017-12-27 16:23:00 -0500

    rebase continued

--- coremods/authtoken/mod_api.py
+++ coremods/authtoken/mod_api.py
@@ -6,134 +6,20 @@ import re
 from flask import request
 
 haxdb = None
-db = None
-config = None
-tools = None
 
 
-def init(app_haxdb, mod_config, mod_tools):
-    global haxdb, db, config, tools
-    haxdb = app_haxdb
-    tools = mod_tools
-    db = haxdb.db
-    config = mod_config
+def init(hdb):
+    global haxdb
+    haxdb = hdb
 
 
 def run():
-    global haxdb, config, db, tools
-
-    @haxdb.app.route("/AUTH/email/login", methods=["GET", "POST"])
-    @haxdb.app.route("/AUTH/email/login/<email>", methods=["GET", "POST"])
-    def mod_auth_email(email=None):
-        email = email or haxdb.get("email")
-        subject = haxdb.get("subject")
-        message = haxdb.get("message")
-        email = email.upper()
-
-        meta = {}
-        meta["api"] = "AUTH/email"
-        meta["action"] = "login"
-        meta["email"] = email
-
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            return haxdb.output(success=0,
-                                message="INVALID VALUE: email",
-                                meta=meta)
-
-        # EMAIL EXISTS%s
-        sql = "SELECT * FROM PEOPLE WHERE PEOPLE_EMAIL = %s"
-        people = db.qaf(sql, (email,))
-        if not people:
-            return haxdb.output(success=0,
-                                value=1,
-                                message="NEW USER",
-                                meta=meta)
-
-        # CREATE TOKEN
-        token = base64.urlsafe_b64encode(os.urandom(500))[5:39]
-        expire = time.time() + int(config["AUTH"]["TOKEN_EXPIRE"])
-        sql = """
-        INSERT INTO AUTH_TOKEN
-        (AUTH_TOKEN_TOKEN, AUTH_TOKEN_PEOPLE_ID, AUTH_TOKEN_EXPIRE)
-        VALUES (%s,%s,%s)
-        """
-        db.query(sql, (token, people["PEOPLE_ID"], expire))
-        if db.error:
-            return haxdb.output(success=0, message=db.error, meta=meta)
-        db.commit()
-
-        to = "{} {} <{}>".format(people["PEOPLE_NAME_FIRST"],
-                                 people["PEOPLE_NAME_LAST"],
-                                 people["PEOPLE_EMAIL"])
-        message = message.replace("[token]", token)
-        result = tools.send_email(to, subject, message)
-        if not result:
-            return haxdb.output(success=0, message=result, meta=meta)
-        return haxdb.output(success=1, message="EMAIL SENT", meta=meta)
-
-    @haxdb.app.route("/AUTH/email/register", methods=["GET", "POST"])
-    @haxdb.app.route("/AUTH/email/register/<email>", methods=["GET", "POST"])
-    def mod_auth_register(email=None):
-        email = email or haxdb.get("email")
-        subject = haxdb.get("subject")
-        message = haxdb.get("message")
-
-        meta = {}
-        meta["api"] = "AUTH/email"
-        meta["action"] = "register"
-        meta["email"] = email
-        meta["subject"] = subject
-        meta["message"] = message
-
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            return haxdb.output(success=0,
-                                message="INVALID VALUE: email",
-                                meta=meta)
-
-        email = email.upper()
-        sql = "SELECT * FROM PEOPLE WHERE PEOPLE_EMAIL = %s"
-        people = db.qaf(sql, (email,))
-        if people:
-            return haxdb.output(success=0,
-                                message="USER ALREADY EXISTS",
-                                meta=meta)
-
-        sql = "INSERT INTO PEOPLE(PEOPLE_EMAIL) VALUES (%s)"
-        db.query(sql, (email,))
-        people_id = db.lastrowid
-        db.commit()
-
-        token = base64.urlsafe_b64encode(os.urandom(500))[5:39]
-        expire = int(time.time()) + int(config["AUTH"]["TOKEN_EXPIRE"])
-        sql = """
-        INSERT INTO AUTH_TOKEN
-        (AUTH_TOKEN_TOKEN, AUTH_TOKEN_PEOPLE_ID, AUTH_TOKEN_EXPIRE)
-        VALUES (%s,%s,%s)
-        """
-        db.query(sql, (token, people_id, expire))
-        if db.error:
-            return haxdb.output(success=0, message=db.error, meta=meta)
-        db.commit()
-
-        to = email
-        message = message.replace("[token]", token)
-        result = tools.send_email(email, subject, message)
-
-        if not result:
-            return haxdb.output(success=0, message=result, meta=meta)
-        return haxdb.output(success=1, message="EMAIL SENT", meta=meta)
-
-    @haxdb.app.route("/AUTH/token", methods=["GET", "POST"])
-    @haxdb.app.route("/AUTH/token/<token>", methods=["GET", "POST"])
+    @haxdb.app.route("/AUTHTOKEN", methods=["GET", "POST"])
+    @haxdb.app.route("/AUTHTOKEN/<token>", methods=["GET", "POST"])
     def mod_auth_token(token=None):
         token = token or haxdb.get("token")
-        config_dbas = [x.strip().upper() for x in config["AUTH"]["DBA"].split(',')]
+        dbas = [x.strip().upper() for x in config["AUTH"]["DBA"].split(',')]
         now = int(time.time())
-
-        meta = {}
-        meta["api"] = "AUTH/email"
-        meta["action"] = "token"
-        meta["token"] = token
 
         # DELETE OLD TOKEN
         db.query("DELETE FROM AUTH_TOKEN WHERE AUTH_TOKEN_EXPIRE<%s", (now,))
@@ -154,7 +40,7 @@ def run():
         row = db.next()
         if not row:
             msg = "TOKEN IS INVALID OR EXPIRED.\nLOG IN AGAIN."
-            return haxdb.output(success=0, message=msg, meta=meta)
+            return haxdb.response(success=0, message=msg)
 
         # CREATE SESSION NODE
         api_key = base64.urlsafe_b64encode(os.urandom(500))[5:39]
