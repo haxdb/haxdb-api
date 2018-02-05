7dd40803fbc2d57beff1a664bd85cf2d92600ee4
jondale <github@serpco.com>
2018-02-05 14:41:25 -0500

    assetnodes api calls

--- coremods/hdb_core_assetnodes/mod_api.py
+++ coremods/hdb_core_assetnodes/mod_api.py
@@ -1,3 +1,5 @@
+from mod_def import mod_def
+
 haxdb = None
 apis = {}
 
@@ -5,6 +7,26 @@ apis = {}
 def init(hdb):
     global haxdb
     haxdb = hdb
+
+    @haxdb.route("/ASSETNODES/list", methods=haxdb.METHOD)
+    def ASSETNODES_list():
+        return haxdb.api.list_call(mod_def["ASSETNODES"])
+
+    @haxdb.route("/ASSETNODES/view", methods=haxdb.METHOD)
+    def ASSETNODES_view():
+        return haxdb.api.view_call(mod_def["ASSETNODES"])
+
+    @haxdb.route("/ASSETNODES/new", methods=haxdb.METHOD)
+    def ASSETNODES_new():
+        return haxdb.api.new_call(mod_def["ASSETNODES"])
+
+    @haxdb.route("/ASSETNODES/delete", methods=haxdb.METHOD)
+    def ASSETNODES_delete():
+        return haxdb.api.delete_call(mod_def["ASSETNODES"])
+
+    @haxdb.route("/ASSETNODES/save", methods=haxdb.METHOD)
+    def ASSETNODES_save():
+        return haxdb.api.save_call(mod_def["ASSETNODES"])
 
 
 def run():
